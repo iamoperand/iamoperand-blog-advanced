c8edf250195d13e5f309e587e24d67520defe2cc
Nikhil Arora <narora200@gmail.com>
2018-09-12 02:26:57 +0530

    [Feat]: Added config file

---
+++ content/meta/config.js
@@ -0,0 +1,32 @@
+const colors = require('../../src/styles/colors');
+
+module.exports = {
+  siteTitle: 'Life Lessons | Not just another blog',
+  shortSiteTitle: 'Life Lessons',
+  siteDescription: 'Life Lessons',
+  siteUrl: 'https://life-lessons.netlify.com',
+  pathPrefix: '',
+  siteImage: '',
+  siteLanguage: 'en',
+  // author
+  authorName: 'Nikhil Arora',
+  authorTwitterAccount: 'iamoperand',
+  // info
+  infoTitle: 'Nikhil',
+  infoTitleNote: 'Life Lessons',
+  // manifest.json
+  manifestName: 'Life Lessons - Not just another blog',
+  manifestShortName: 'Life Lessons', // max 12 characters
+  manifestStartUrl: '/',
+  manifestBackgroundColor: colors.background,
+  manifestThemeColor: colors.background,
+  manifestDisplay: 'standalone',
+  // contact
+  contactEmail: 'yourlifelessons.official@gmail.com',
+  // social
+  authorSocialLinks: [
+    { name: 'github', url: 'https://github.com/iamoperand' },
+    { name: 'twitter', url: 'https://twitter.com/iamoperand' },
+    { name: 'facebook', url: 'http://facebook.com/iamoperand' },
+  ],
+};
